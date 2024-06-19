fe28efadb46ca3dc6551bd22316f4c8681bbfbfc
T_Peng <59012776+Rin-lr@users.noreply.github.com>
2024-06-19 17:39:08 +0300

    Parsing is working
    Web parsing is working. atm shows only one part out of many.

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-const RSS_URL = `https://flipboard.com/@raimoseero/feed-nii8kd0sz.rss`;
+document.getElementById('parse-button').addEventListener('click', function() {
+  var articleUrl = document.getElementById('article-url').value;
+  fetch('/webparser', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      // Add any other headers required by the API
+    },
+    body: JSON.stringify({
+      url: articleUrl
+    })
+  })
+    .then(response => response.json())
+    .then(data => {
+      // Data = JSON returned from the API
+      document.getElementById('article-title').textContent = data.title;
+      document.getElementById('article-img').src = data.lead_image_url;
+      document.getElementById('article-description').textContent = data.excerpt;
 
-fetch(RSS_URL)
-  .then(response => response.text())
-  .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-  .then(data => console.log(data))
+    })
+
+  .catch(error => console.error('Error:', error));
+});
+
--- server.js
+++ server.js
@@ -1,12 +1,36 @@
-//Bypass fetching problem. CORS policy.
-const express = require('express');
-const request = require('request');
+import path from 'path';
+import { fileURLToPath } from 'url';
+import express from 'express';
+import fetch from 'node-fetch';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 
-app.use('/proxy', (req, res) => {
-  const url = "https://flipboard.com/@raimoseero/feed-nii8kd0sz.rss" + req.url;
-  req.pipe(request(url)).pipe(res);
+app.use(express.json());
+
+app.post('/webparser', (req, res) => {
+  const articleUrl = req.body.url;
+  fetch('https://uptime-mercury-api.azurewebsites.net/webparser', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      // Add any other headers required by the API
+    },
+    body: JSON.stringify({
+      url: articleUrl
+    })
+  })
+  .then(apiResponse => apiResponse.json())
+  .then(data => res.json(data))
+  .catch(error => res.status(500).json({ error: error.message }));
 });
 
-app.listen(process.env.PORT || 3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.use(express.static(__dirname));
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: __dirname }); 
+});
+
+
